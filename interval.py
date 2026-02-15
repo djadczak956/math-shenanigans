b9278c1ffad3879e28f39aa2c348b8fc88ce745b
Damian Jadczak <62266703+djadczak956@users.noreply.github.com>
2026-02-15 14:52:28 -0500

    Added method descriptions, refactored a bit

--- interval.py
+++ interval.py
@@ -1,22 +1,36 @@
 class interval:
-    def __init__(self, left_bracket: bool, right_bracket: bool, 
+    def __init__(self, left_bound_type: int, right_bound_type: int, 
                  left_bound: float, right_bound: float):
-        self.left_bracket = left_bracket
-        self.right_bracket = right_bracket
+        """Initialize interval with bound types and their numerical bounds. 
+
+        left_bound_type -- 0 for open bound, 1 for closed bound
+        right_bound_type -- 0 for open bound, 1 for closed bound
+        left_bound -- numerical value of left bound
+        right_bound -- numerical value of right bound
+        """
+
+        self.left_bound_type = left_bound_type
+        self.right_bound_type = right_bound_type
         self.left_bound = left_bound
         self.right_bound = right_bound
 
     def set_bounds(self, left_bound: float, right_bound: float) -> None:
-        try:
-            self.left_bound = left_bound
-            self.right_bound = right_bound
-        except:
-            print("Error: Bounds not set.")
+        """Set the numerical bounds of an interval.
+
+        Keyword arguments: 
+        left_bound -- numerical value of new left bound
+        right_bound -- numerical value of new right bound
+        """
+
+        self.left_bound = left_bound
+        self.right_bound = right_bound
 
     def disp_bounds(self) -> str:
-        # Choose whether each bound is open or closed
-        lb = ["(", "["][self.left_bracket]  
-        rb = [")", "]"][self.right_bracket]
+        """Return a string displaying the interval in mathematical notation."""
+        
+        # Choose proper bracket for each bound
+        lb = ["(", "["][self.left_bound_type]  
+        rb = [")", "]"][self.right_bound_type]
 
         return f"{lb}{self.left_bound}, {self.right_bound}{rb}"
     
