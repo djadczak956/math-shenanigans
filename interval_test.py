90f5781120cd7d3a4301a01c4247e9a67ba642a1
Damian Jadczak <62266703+djadczak956@users.noreply.github.com>
2026-02-16 19:47:11 -0500

    Some tests to make sure checks work

--- interval_test.py
+++ interval_test.py
@@ -1,6 +1,6 @@
 from interval import interval
 
-
+# Test basic construction
 x1 = interval(0, 0, 0, 1)
 print(x1.disp_bounds())
 
@@ -18,3 +18,7 @@ for a in [-1, 0, 0.25, 1/3, 0.5, 2/3, 0.75, 0.999999, 1, 1.01]:
 for i in range(1, 21):
     num = float("0." + i * "9")
     print(f"({i} digit(s)): {num} in {x1.disp_bounds()}: {x1.contains(num)}")
+
+# Test bounds checking
+x1.set_bounds(5, 0)
+print(x1.disp_bounds())
