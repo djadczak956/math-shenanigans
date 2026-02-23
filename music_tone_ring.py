6035d202d879f8d736add7d8947da570873b151a
Damian <jadczakdamian@gmail.com>
2026-02-23 13:27:44 -0500

    Added user input and check for root note

--- music_tone_ring.py
+++ music_tone_ring.py
@@ -1,8 +1,22 @@
-
+import numpy as np
 
 z12 = list(range(0, 12))
-tones = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-tone_vals = dict(zip(tones, z12))
+base_tones = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+#
+while True:
+    root = input("Pick a root note: ")
+    if root in base_tones:
+        print(f"Selected {root} as root note.")
+        break
+    else:
+        print("Try again (pick one of the following: 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'). ")
 
+tones = base_tones
+tone_vals = dict(zip(z12, tones))
 
+def add_table() -> None:
+    pass
+
+def mult_table() -> None:
+    pass
 
