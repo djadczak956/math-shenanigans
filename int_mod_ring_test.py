df451030681947c68b75e60bee6bb55932bda5c3
Damian <jadczakdamian@gmail.com>
2026-02-23 14:02:47 -0500

    Ran a test with a very large ring

---
+++ int_mod_ring_test.py
@@ -0,0 +1,21 @@
+from int_mod_ring import int_mod_ring
+import time
+
+z2 = int_mod_ring(2)
+print(z2.to_list())
+
+# for i in range(2, 1000):
+#     print(f"Z mod {i}: ", end="") 
+#     start = time.time()
+#     ring = int_mod_ring(i).to_list()
+#     end = time.time()
+#     print(ring)
+#     print(f"Took {end} - {start} = {end - start}s")
+
+# This takes over a minute on my machine
+# BIG_NUM = 1000000000
+# print(f"Z mod {BIG_NUM}: ", end="") 
+# start = time.time()
+# ring = int_mod_ring(BIG_NUM).to_list()
+# end = time.time()
+# print(f"Took {end} - {start} = {end - start}s")
